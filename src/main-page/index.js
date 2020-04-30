d84b5d44c0539ffdf3cd6c143d3d740cb44e9f76
Kremifany <34751960+Kremifany@users.noreply.github.com>
2020-04-30 16:43:39 -0400

    Initial commit

---
+++ src/main-page/index.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './main-page.css';
+import Header from './header.js';
+
+class App extends Component {
+  render() {
+      return (
+        <div className="container">
+          <Header/>
+        </div>
+    );
+  }
+}
+export default App;
