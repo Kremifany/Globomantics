6b4f42df3b39ec9328ea3cebe6d7db88bb40ed6b
Kremifany <34751960+Kremifany@users.noreply.github.com>
2020-04-30 23:34:26 -0400

    House and Featured House components are added

--- src/main-page/index.js
+++ src/main-page/index.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './main-page.css';
 import Header from './header.js';
+import FeaturedHouse from './featured-house';
 
 class App extends Component {
+
+  state = {}
+
+  componentDidMount(){
+    this.fetchHouses();
+  }
+
+  fetchHouses = () =>{
+    fetch('/houses.json')
+    .then(rsp => rsp.json())
+    .then(allHouses => {
+      this.allHouses = allHouses;
+      this.determineFeaturedHouse();
+    })
+  }
+
+determineFeaturedHouse = () => {
+  if(this.allHouses){
+    const randomIndex = Math.floor(Math.random() * this.allHouses.length);
+    const featuredHouse = this.allHouses[randomIndex];
+    this.setState({ featuredHouse });
+  };
+}
+
+
   render() {
       return (
         <div className="container">
-          <Header/>
+          <Header subtitle="Providing houses world wide"/>
+          <FeaturedHouse house={this.state.featuredHouse}/>
         </div>
     );
-  }
+  } 
 }
 export default App;
